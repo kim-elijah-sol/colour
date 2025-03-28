45f77c0d2e5a474a938c23fcbb0eb4a048f1c787
kim-elijah-sol <kimelijahsol@gmail.com>
2025-03-28 12:46:49 +0000

    feature/create: preview > gradient length change

--- src/domain/create/components/CreatePreview/Previews/Gradient/index.tsx
+++ src/domain/create/components/CreatePreview/Previews/Gradient/index.tsx
@@ -14,10 +14,10 @@ function Gradient() {
       className={style.container}
       style={{
         background: `
-            linear-gradient(135deg, #${colors[0]}, ${alphaZeroColors[0]} 70.71%),
-            linear-gradient(225deg, #${colors[1]}, ${alphaZeroColors[1]} 70.71%),
-            linear-gradient(315deg, #${colors[2]}, ${alphaZeroColors[2]} 70.71%),
-            linear-gradient(45deg,  #${colors[3]}, ${alphaZeroColors[3]} 70.71%)`,
+            linear-gradient(135deg, #${colors[0]}, ${alphaZeroColors[0]} 60.71%),
+            linear-gradient(225deg, #${colors[1]}, ${alphaZeroColors[1]} 60.71%),
+            linear-gradient(315deg, #${colors[2]}, ${alphaZeroColors[2]} 60.71%),
+            linear-gradient(45deg,  #${colors[3]}, ${alphaZeroColors[3]} 60.71%)`,
       }}
     ></div>
   );
