1ac369f7f0d92e618803be3c1f8ac3fa30dbb739
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-05 10:29:18 +0000

    feature/router: add favourite page

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import Index from '@/pages';
 import Create from '@/pages/create';
 import Popular from '@/pages/popular';
+import Favourite from './pages/favourite';
 
 import Layout from '@/components/layouts/Layout';
-import ModalProvider from '@/utils/components/Modal/ModalProvider';
 
 import '@/styles/reset.css';
 import { themeClass } from './styles/theme.css';
@@ -24,19 +24,18 @@ const queryClient = new QueryClient({
 function Router() {
   return (
     <div className={themeClass}>
-      <ModalProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route index element={<Index />} />
-                <Route path='/popular' element={<Popular />} />
-                <Route path='/create' element={<Create />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </QueryClientProvider>
-      </ModalProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route index element={<Index />} />
+              <Route path='/popular' element={<Popular />} />
+              <Route path='/create' element={<Create />} />
+              <Route path='/favourite' element={<Favourite />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
---
+++ src/pages/favourite.tsx
@@ -0,0 +1,5 @@
+function Favourite() {
+  return <></>;
+}
+
+export default Favourite;
