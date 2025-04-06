272f267c8a93b3f36a101c8c05a0561b1432f031
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-06 09:03:50 +0000

    chore/fe: goorm ide host setting

--- apps/colour-fe/vite.config.ts
+++ apps/colour-fe/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
   server: {
     host: true,
-    allowedHosts: ['solreacttest.run.goorm.io'],
+    allowedHosts: ['colour-fe.run.goorm.io'],
   },
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
