c22fa74a2966853f314e3645d86f813b306d99d9
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-09 11:17:41 +0000

    @fe/feature/sign-in: tko sign-in

--- apps/colour-fe/src/domain/sign-in/SignInModal/hooks/useHandlePasswordWhenSignIn.ts
+++ apps/colour-fe/src/domain/sign-in/SignInModal/hooks/useHandlePasswordWhenSignIn.ts
@@ -1,17 +1,26 @@
+import { useModalShowContext } from '@/stores/ModalShowContext';
 import useSignInStore from '@/stores/useSignInStore';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { HTTPError } from 'ky';
 import { postSignIn } from '../apis/postSignIn';
 
 function useHandlePasswordWhenSignIn() {
+  const queryClient = useQueryClient();
+
+  const { close } = useModalShowContext();
+
   const { email, password } = useSignInStore();
 
   const { mutate } = useMutation({
     mutationKey: ['postSignIn'],
     mutationFn: postSignIn,
     onSuccess: (data) => {
-      // Success Data : Tokens
-      console.log(data.data);
+      close();
+
+      localStorage.setItem('colour-access-token', data.data.accessToken);
+      localStorage.setItem('colour-refresh-token', data.data.refreshToken);
+
+      queryClient.invalidateQueries();
     },
     onError: async (error) => {
       if (error instanceof HTTPError) {
