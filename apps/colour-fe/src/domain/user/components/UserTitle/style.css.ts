76b9d1d6842c94bbe6a3314d924e4fc3caaac797
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-10 14:46:20 +0000

    @fe/feature/user: user title font set to `IBM Plex Sans`

--- apps/colour-fe/src/domain/user/components/UserTitle/style.css.ts
+++ apps/colour-fe/src/domain/user/components/UserTitle/style.css.ts
@@ -2,7 +2,9 @@ import { margin } from '@/utils/styles';
 import { style } from '@vanilla-extract/css';
 
 export const userTitle = style({
-  fontSize: 36,
+  fontSize: 42,
   fontWeight: 800,
   ...margin({ bottom: 16, x: 8 }),
+  fontFamily: 'IBM Plex Sans',
+  color: '#000000',
 });
