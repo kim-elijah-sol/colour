2c306624439904cb24077fa5d8b93b27e3a48353
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-11 11:21:27 +0000

    @fe/design/user/account: form

--- apps/colour-fe/src/domain/user/pages/user-account/index.tsx
+++ apps/colour-fe/src/domain/user/pages/user-account/index.tsx
@@ -1,6 +1,18 @@
+import * as signInModalStyles from '@/domain/sign-in/SignInModal/components/style.css';
+import usePasswordValidator from '@/hooks/usePasswordValidator';
+import { useState } from 'react';
 import userContentStyles from '../../styles/user-content-styles.css';
 
 function UserAccount() {
+  const [email, setEmail] = useState('');
+
+  const [currentPassword, setCurrentPassword] = useState('');
+
+  const [newPassword, setNewPassword] = useState('');
+
+  const { length, letterAndNumber, specialChar } =
+    usePasswordValidator(newPassword);
+
   return (
     <>
       <h2 className={userContentStyles.title}>Account</h2>
@@ -9,6 +21,104 @@ function UserAccount() {
         <br />
         Change your email or password whenever you need.
       </p>
+
+      <div className={userContentStyles.sectionList}>
+        <section className={userContentStyles.section}>
+          <h3 className={userContentStyles.sectionTitle}>Email</h3>
+          <form className={userContentStyles.form}>
+            <input
+              type='email'
+              placeholder='your email'
+              className={userContentStyles.input}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type='submit' className={userContentStyles.button}>
+              Save
+            </button>
+          </form>
+        </section>
+        <section className={userContentStyles.section}>
+          <h3 className={userContentStyles.sectionTitle}>Password</h3>
+          <form className={userContentStyles.form}>
+            <input
+              type='password'
+              placeholder='current password'
+              className={userContentStyles.input}
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+            />
+            <div>
+              <input
+                type='password'
+                placeholder='new password'
+                className={userContentStyles.input}
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+              <div className={signInModalStyles.passwordGuideWrapper}>
+                <div className={signInModalStyles.passwordGuideItem}>
+                  <div
+                    className={
+                      signInModalStyles.passwordGuideCircle[
+                        length ? 'pass' : 'nonePass'
+                      ]
+                    }
+                  />
+                  <p
+                    className={
+                      signInModalStyles.passwordGuideText[
+                        length ? 'pass' : 'nonePass'
+                      ]
+                    }
+                  >
+                    8 ~ 16 characters
+                  </p>
+                </div>
+                <div className={signInModalStyles.passwordGuideItem}>
+                  <div
+                    className={
+                      signInModalStyles.passwordGuideCircle[
+                        letterAndNumber ? 'pass' : 'nonePass'
+                      ]
+                    }
+                  />
+                  <p
+                    className={
+                      signInModalStyles.passwordGuideText[
+                        letterAndNumber ? 'pass' : 'nonePass'
+                      ]
+                    }
+                  >
+                    letter & number
+                  </p>
+                </div>
+                <div className={signInModalStyles.passwordGuideItem}>
+                  <div
+                    className={
+                      signInModalStyles.passwordGuideCircle[
+                        specialChar ? 'pass' : 'nonePass'
+                      ]
+                    }
+                  />
+                  <p
+                    className={
+                      signInModalStyles.passwordGuideText[
+                        specialChar ? 'pass' : 'nonePass'
+                      ]
+                    }
+                  >
+                    special character
+                  </p>
+                </div>
+              </div>
+            </div>
+            <button type='submit' className={userContentStyles.button}>
+              Save
+            </button>
+          </form>
+        </section>
+      </div>
     </>
   );
 }
--- apps/colour-fe/src/domain/user/styles/user-content-styles.css.ts
+++ apps/colour-fe/src/domain/user/styles/user-content-styles.css.ts
@@ -1,4 +1,5 @@
-import { margin } from '@/utils/styles';
+import { vars } from '@/styles/theme.css';
+import { brandButtonCSS, flex, margin, padding } from '@/utils/styles';
 import { style } from '@vanilla-extract/css';
 
 const userContentStyles = {
@@ -12,8 +13,56 @@ const userContentStyles = {
     fontWeight: 500,
     lineHeight: '32px',
     color: '#9AABB7',
-    ...margin({ bottom: 16 }),
+    ...margin({ bottom: 40 }),
   }),
+  sectionList: style([
+    flex({ direction: 'column' }),
+    {
+      gap: 32,
+    },
+  ]),
+  section: flex({ direction: 'column' }),
+  sectionTitle: style({
+    fontSize: 22,
+    fontWeight: 700,
+    ...margin({ bottom: 20 }),
+    borderBottom: '1px solid #EEEEEE',
+  }),
+  input: style({
+    width: vars.full,
+    height: 46,
+    lineHeight: '46px',
+    fontSize: 16,
+    fontWeight: 600,
+    ...padding({ x: 16 }),
+    color: '#333',
+    border: '1px solid #E0E0E0',
+    borderRadius: 12,
+    transition: '0.21s',
+    ':focus': {
+      borderColor: vars.color.brand,
+    },
+    '::placeholder': {
+      color: '#BBBBBB',
+    },
+  }),
+  form: style([
+    flex({ direction: 'column' }),
+    {
+      gap: 16,
+    },
+  ]),
+  button: style([
+    brandButtonCSS,
+    {
+      fontWeight: 600,
+      fontSize: 16,
+      ...padding({ x: 16 }),
+      height: 40,
+      borderRadius: 12,
+      alignSelf: 'flex-end',
+    },
+  ]),
 } as const;
 
 export default userContentStyles;
