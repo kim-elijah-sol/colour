916b4ffeca39c23e80f47d4aeaacc8a4f5b8148d
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-21 14:00:19 +0000

    @be/fix/colour: CreatePaletteRequestDTO remove validate

--- apps/colour-be/src/domain/colour/dtos/CreatePaletteRequest.dto.ts
+++ apps/colour-be/src/domain/colour/dtos/CreatePaletteRequest.dto.ts
@@ -1,24 +1,8 @@
-import { Type } from 'class-transformer';
-import {
-  ArrayMaxSize,
-  ArrayMinSize,
-  IsArray,
-  IsString,
-  Matches,
-  ValidateNested,
-} from 'class-validator';
-
-class HexColorDTO {
-  @IsString()
-  @Matches(/^[0-9a-fA-F]{6}$/)
-  value: string;
-}
+import { ArrayMaxSize, ArrayMinSize, IsArray } from 'class-validator';
 
 export class CreatePaletteRequestDTO {
   @IsArray()
   @ArrayMinSize(4)
   @ArrayMaxSize(4)
-  @ValidateNested({ each: true })
-  @Type(() => HexColorDTO)
-  colour: HexColorDTO[];
+  colour: string[];
 }
