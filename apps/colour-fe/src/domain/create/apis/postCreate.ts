22f3cee1caeb7e9f712119d6f5e4cb7dfcab9782
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-27 13:32:15 +0000

    @fe/refactoring: remove unnecessary files

--- apps/colour-fe/src/domain/create/apis/postCreate.ts
+++
@@ -1,9 +0,0 @@
-export type PostCreateRequest = {
-  colors: string[];
-};
-
-export const postCreate = (body: PostCreateRequest) =>
-  fetch('/create', {
-    method: 'POST',
-    body: JSON.stringify(body),
-  }).then((res) => res.json());
