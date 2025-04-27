c8ec5ff2d6082c90fc2c464e76679843a80d3f04
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-27 14:24:32 +0000

    @fe/remove: mock server

--- apps/colour-fe/src/main.tsx
+++ apps/colour-fe/src/main.tsx
@@ -1,13 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
 
-async function enableMocking() {
-  const { worker } = await import('../server/browsers');
-
-  return worker.start();
-}
-
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('colour')!).render(<Router />);
-});
+ReactDOM.createRoot(document.getElementById('colour')!).render(<Router />);
